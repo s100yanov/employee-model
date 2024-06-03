67acd03f38bf974ae8eb131f742d297d33972ea2
Svetoslav Stoyanov <55459796+s100yanov@users.noreply.github.com>
2024-06-03 19:00:43 +0300

    Update Main.java
    A few brief changes in the driver method after the change in the Employee class. Just another Object created used for functionality check.

--- Main.java
+++ Main.java
@@ -10,6 +10,7 @@ public class Main {
 
         Employee Victor = new Employee("Victor", "Stefanov", "18/03/1985", 12.5);
         Employee Maria = new Employee("Maria", "Petrova", "14/04/1984", 15);
+        Employee Alexander = new Employee("Alexander", "Yordanov", "26/03/1986", 14);
         Expert Daniel = new Expert("Daniel", "Stefanov", "18/10/1976", 23, "Operator", "Legal");
         Expert Boris = new Expert("Boris", "Dimitrov", "06/08/1991", 4, "Operator", "Technical");
         Expert Iva = new Expert("Iva", "Nikolova", "25/08/1993", 8.5, "Manager", "Legal");
@@ -28,33 +29,39 @@ public class Main {
         System.out.println("---------------------------------------------------------------");
 
         System.out.println("Maria:");
-        System.out.println(Maria);
-        System.out.println(Maria.getDateOfBirth());
+        System.out.println("Brief Profile : " + Maria);
+        System.out.println("Date of Birth: " + Maria.getDateOfBirth());
         System.out.println("Boris:");
-        System.out.println(Boris.id());
-        System.out.println(Boris.fullId());
-        System.out.println(Boris.getEMailAddress());
-        System.out.println(Boris.getDepartment());
-        System.out.println(Boris.getDivision());
-        System.out.println(Boris.getPosition());
+        System.out.println("ID: " + Boris.id());
+        System.out.println("Full ID: " + Boris.fullId());
+        System.out.println("E-Mail: " + Boris.getEMailAddress());
+        System.out.println("Department: " + Boris.getDepartment());
+        System.out.println("Division: " + Boris.getDivision());
+        System.out.println("Position: " + Boris.getPosition());
         System.out.println("Iva:");
-        System.out.println(Iva.getPosition());
-        System.out.println(Iva.getExpertise());
-        System.out.println(Iva.getDepartment());
-        System.out.println(Iva.getDivision());
+        System.out.println("Position: " + Iva.getPosition());
+        System.out.println("Expertise: " + Iva.getExpertise());
+        System.out.println("Department: " + Iva.getDepartment());
+        System.out.println("Division: " + Iva.getDivision());
 
         System.out.println("---------------------------------------------------------------");
 
         List<Employee> subordinates = new ArrayList<>(List.of(Victor, Maria, Daniel));
-        Iva.setSubordinates(subordinates);
+        Iva.setSubordinates(subordinates, Iva);
         System.out.println("Victor's superior:   " + Victor.getSuperior());
         System.out.println("Iva's subordinates:   " + Iva.getSubordinates());
 
         Iva.setSuperior(Elena, Iva);
-        Ivan.setSubordinates(Elena);
+        Ivan.setSubordinates(Elena, Ivan);
         System.out.println("Iva's superior:   " + Iva.getSuperior());
         System.out.println("Ivan's subordinates:   " + Ivan.getSubordinates());
-        System.out.println("Boris' salary: " + Boris.checkRandomEmployeeSalary(Boris));
+        Elena.setSubordinates(Alexander, Elena);
+        System.out.println("Elena's subordinates:   " + Elena.getSubordinates());
+        System.out.println("Alexander's superior:   " + Alexander.getSuperior());
+
+        System.out.println("---------------------------------------------------------------");
+
+        System.out.println("Director Elena is checking employee Boris' salary: " + Elena.checkRandomEmployeeSalary(Boris));
 
         System.out.println("---------------------------------------------------------------");
 
